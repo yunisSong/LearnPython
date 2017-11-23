9eedcce160ee6fa3e625169e579f53c40534acc1
yunis <332963965@qq.com>
2017-11-23 17:13:54 +0800

    广度搜索优先
    广度搜索优先

---
+++ 算法图解/graph.py
@@ -0,0 +1,20 @@
+from collections import deque
+
+graph = {}
+graph["you"] = ["alice", "bob", "claire"]
+graph["bob"] = ["anuj", "peggy"]
+graph["alice"] = ["peggy"]
+graph["claire"] = ["thom", "jonny"]
+graph["anuj"] = []
+graph["peggy"] = []
+graph["thom"] = []
+graph["jonny"] = []
+
+
+search_queue = deque()
+search_queue += graph["you"]
+
+while search_queue:
+    
+    pass
+return False
